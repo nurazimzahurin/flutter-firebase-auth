e59d534622f913439291e9dfb504d37ab232134e
azimzahurin <nurazimzahurin@raudhahpay.com>
2020-06-14 22:47:35 +0800

    fix overlay

--- lib/pages/authenticate/register.dart
+++ lib/pages/authenticate/register.dart
@@ -23,75 +23,77 @@ class _RegisterState extends State<Register> {
         title: Text('Sign Up'),
         backgroundColor: Colors.blueAccent,
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
-        child: Form(
-          key: _formKey,
-            child: Column(
-          children: <Widget>[
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Email',
-                fillColor: Colors.blueAccent,
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2)
+      body: SingleChildScrollView(
+              child: Container(
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+          child: Form(
+            key: _formKey,
+              child: Column(
+            children: <Widget>[
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Email',
+                  fillColor: Colors.blueAccent,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 2)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width:1)
+                  )
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width:1)
-                )
+                validator: (val) => val.isEmpty ? 'Email is required' : null,
+                onChanged: (val) {
+                  setState(() {
+                    email = val;
+                  });
+                },
               ),
-              validator: (val) => val.isEmpty ? 'Email is required' : null,
-              onChanged: (val) {
-                setState(() {
-                  email = val;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            TextFormField(
-              decoration: InputDecoration(
-                hintText: 'Password',
-                fillColor: Colors.blueAccent,
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.blueAccent, width: 2)
+              SizedBox(height: 20),
+              TextFormField(
+                decoration: InputDecoration(
+                  hintText: 'Password',
+                  fillColor: Colors.blueAccent,
+                  filled: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.blueAccent, width: 2)
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white, width:1)
+                  )
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.white, width:1)
-                )
+                validator: (val) => val.length < 6 ? 'password must be more than 6 character' : null,
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() {
+                    password = val;
+                  });
+                },
               ),
-              validator: (val) => val.length < 6 ? 'password must be more than 6 character' : null,
-              obscureText: true,
-              onChanged: (val) {
-                setState(() {
-                  password = val;
-                });
-              },
-            ),
-            SizedBox(height: 20),
-            RaisedButton(
-              onPressed: () async {
-                if(_formKey.currentState.validate()){
-                  dynamic result = await _auth.register(email, password);
-                  if(result == null){
-                    setState(() {
-                      error = 'Invalid email';
-                    });
+              SizedBox(height: 20),
+              RaisedButton(
+                onPressed: () async {
+                  if(_formKey.currentState.validate()){
+                    dynamic result = await _auth.register(email, password);
+                    if(result == null){
+                      setState(() {
+                        error = 'Invalid email';
+                      });
+                    }
                   }
-                }
-              },
-              child: Text(
-                'Sign Up',
-                style: TextStyle(color: Colors.white),
+                },
+                child: Text(
+                  'Sign Up',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue[900],
               ),
-              color: Colors.blue[900],
-            ),
-            SizedBox(height:12),
-            Text(error, style: TextStyle(color: Colors.red, fontSize:14)),
-          ],
-        )),
+              SizedBox(height:12),
+              Text(error, style: TextStyle(color: Colors.red, fontSize:14)),
+            ],
+          )),
+        ),
       ),
     );
   }
